c8f2cdbdc69348c48351e4b6abb2d9f6c644c910
AlexMadM <alex.alexandrov.1988@gmail.com>
2024-09-25 22:15:50 +0300

    working on productPage

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,3 +1,14 @@
-export default function ProductPage({ params: { id } }: { params: { id: string } }) {
-  return <div>ProductPage {id}</div>
+import {prisma} from "@/prisma/prisma-client";
+import {notFound} from "next/navigation";
+import {Container} from "@/components/shared/container";
+
+export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
+
+  const product = await prisma.product.findFirst({where: {id:Number(id)}});
+if (!product) {
+  return notFound();
+}
+  return <Container className='flex flex-col my-10'>
+    <ProductImage src={product.imageUrl} />
+  </Container>
 }
