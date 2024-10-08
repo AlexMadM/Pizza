842d8af17ef1d264bc0f8aa7e9e0d6ff90f83cc8
AlexMadM <alex.alexandrov.1988@gmail.com>
2024-10-08 23:27:35 +0300

    add trash

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,12 +1,41 @@
+import { prisma } from '@/prisma/prisma-client'
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 export async function GET(req: NextRequest) {
   try {
     const userId = 1
+    const token = req.cookies.get('cartToken')?.value
+    if (!token) {
+      return NextResponse.json({ totalAmount: 0, items: [] })
+    }
 
-    return NextResponse.json({ cart: [] })
+    const userCart = await prisma.cart.findFirst({
+      where: {
+        OR: [
+          {
+            tokenId: token,
+          },
+        ],
+      },
+      include: {
+        items: {
+          orderBy: {
+            createdAt: 'desc',
+          },
+          include: {
+            productItem: {
+              include: {
+                product: true,
+              },
+            },
+            ingredients: true,
+          },
+        },
+      },
+    })
+
+    return NextResponse.json(userCart)
   } catch (error) {
     console.log(error)
   }
-  return NextResponse.json({ cart: [] })
 }
